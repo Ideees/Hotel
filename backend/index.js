c81c78a1985ad10e8585f51e7c8cf2ccc48fd60d
M IDREES FAIZ <91368617+Ideees@users.noreply.github.com>
2024-05-11 10:16:13 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,9 @@ mongoose
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
+app.get("/", (req, res) => {
+    res.json("hello idrees");
+})
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
