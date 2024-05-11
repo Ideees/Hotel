dd39781729f8a9f0aa22f2b584a325a05152fc9c
M IDREES FAIZ <91368617+Ideees@users.noreply.github.com>
2024-05-11 12:24:19 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 // Middleware setup
 app.use(
   cors({
-    origin: ["https://hotel-front-taupe.vercel.app"],
+    origin: [res.setHeader('Access-Control-Allow-Origin', 'https://hotel-front-taupe.vercel.app')],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   })
